8b9557d3e05f58664c389b04dcfe66d656b83ca7
Yunfei Guo <yunfeigu@usc.edu>
2014-10-03 04:38:34 -0700

    ROC function added, but not fully tested

--- source/analyze_sampleBVS_output.R
+++ source/analyze_sampleBVS_output.R
@@ -1,24 +1,82 @@
-args <- commandArgs(TRUE)
-file = args[1]
-#format:
-#N	TIME	GLOBAL_BF	BF1	BF2...
-data = read.table(file,fill=TRUE,header=FALSE)
-total = length(data[,2])
-success =sum(data[,2]!="ERROR") 
-fail =sum(data[,2]=="ERROR") 
-
-cat("Total simulations:",total,"\n")
-if(total != 0)
+require('pROC')
+##########################SUBROUTINES###################################
+make_roc <- function(file="roc.pdf",names,data,outcome)
 {
-    cat("Successful simulations:",success,"(",success/total,")\n")
-    cat("Failed simulations:",fail,"(",fail/total,")\n")
-    if(dim(data)[2]>=4)
+    pdf(file)
+    auc.val = NULL
+    for(i in 1:length(args))
     {
-	n_hiBF = sum(apply(data[data[,2]!="ERROR",],1,function(i){i=as.numeric(i);max(i[4:length(i)])==i[4]}))
-	cat("Simulations with 1st region having highest BF:", n_hiBF,"(",n_hiBF/total,")\n")
-	cat("Global BF:\n")
-	print(summary(data[,3]))
-	cat("BF1:\n")
-	print(summary(data[,4]))
+	roc.obj = roc(outcome,data[i,])
+	if(i == 1)
+	{
+	    plot(roc.obj,col=col[i])
+	} else
+	{
+	    lines.roc(roc.obj,col=col[i])
+	}
+	auc.val = c(auc.val,roc.obj$auc)
     }
+    legend("bottomright", legend=paste(args,"AUC:",format(auc.val,digits=2)),
+	   col=col, lwd=2)
+    dev.off()
+}
+########################MAIN#############################################
+args <- commandArgs(TRUE)
+globalBF=NULL
+RBF=NULL
+margBF=NULL
+if(length(args)>0)
+{
+    for (i in 1:length(args))
+    {
+	#format:
+	#N	TIME	GLOBAL_BF	10	REGIONAL_BF	BF1	BF2...	MARGINAL_BF	BF1	BF2...
+	file = args[i]
+	data = read.table(file,fill=TRUE,header=FALSE)
+	success.idx = data[,2]!="ERROR"
+
+	globalBF.idx = which(data[success.idx,][1,] == c('GLOBAL_BF'))
+	RBF.idx = which(data[success.idx,][1,] == c('REGIONAL_BF'))
+	margBF.idx = which(data[success.idx,][1,] == c('MARGINAL_BF'))
+	stopifnot(margBF.idx>RBF.idx)
+
+	globalBF.idx = globalBF.idx + 1
+	RBF.idx = (RBF.idx+1):(margBF.idx-1)
+	margBF.idx = (margBF.idx+1):(dim(data)[2])
+	globalBF = rbind(globalBF,data[success.idx,globalBF.idx])
+	RBF = rbind(RBF,apply(data[success.idx,RBF.idx],2,median))
+	margBF = rbind(margBF,apply(data[success.idx,margBF.idx],2,median))
+
+	total = length(data[,2])
+	success =sum(success.idx) 
+	fail =sum(!success.idx) 
+
+	cat("File:",file,"\n")
+	cat("Total simulations:",total,"\n")
+	if(total != 0)
+	{
+	    cat("Successful simulations:",success,"(",100*success/total,"%)\n")
+	    cat("Failed simulations:",fail,"(",100*fail/total,"%)\n")
+	    if(dim(data)[2]>=6)
+	    {
+		n_hiBF = sum(apply(data[success.idx,RBF.idx],1,function(i){i=as.numeric(i);max(i)==i[1]}))
+		cat("Simulations with 1st region having highest BF:", n_hiBF,"(",100*n_hiBF/total,"%)\n")
+		cat("Global BF:\n")
+		print(summary(data[success.idx,globalBF.idx]))
+		cat("BF1:\n")
+		print(summary(data[success.idx,RBF.idx][,1]))
+	    }
+	}
+    }
+
+    #plot ROC reports
+    col = rainbow(length(args))
+    #globalBF, assume first half is associated, second half is not
+    #stopifnot(dim(globalBF)[2] % 2 == 0)
+    #make_roc(file="globalBF_ROC.pdf",names=args,data=globalBF,outcome=c(rep(1,dim(globalBF)[2]/2),rep(0,dim(globalBF)[2]/2)))
+    #RBF(assume 1st region is always associated risk, no other region is associated with risk)
+    make_roc(file="RBF_ROC.pdf",names=args,data=RBF,outcome=c(1,rep(0,dim(RBF)[2]-1)))
+    #marginal BF (each variant), assume only last 10 variants are risk variants
+    stopifnot(dim(margBF)[2]>=10)
+    make_roc(file="margBF_ROC.pdf",names=args,data=margBF,outcome=c(rep(0,dim(margBF)[2]-10),rep(1,10)))
 }
